af380274c6d30bcc7dd10f8925811dc31cd4011a
Philips Xu <philips.ilips@gmail.com>
2021-09-05 21:41:55 -0400

    extracted image details

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { FoodEntry } from "../src/types/FoodEntry";
 import Hyperlink from "./Hyperlink";
 
@@ -7,27 +7,35 @@ type Props = {
   item: FoodEntry;
 };
 
+const Details: FunctionComponent<Props> = ({ item }: Props) => {
+  return (
+    <div className="mt-3">
+      <p className="text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
+        {item.dateCreated.toLocaleDateString("en-US")}
+      </p>
+      <p className="mt-2 text-lg leading-6 font-semibold">{item.title}</p>
+      {item.description != null && (
+        <p className="mt-2 text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
+          {item.description}
+        </p>
+      )}
+      {item.source != null && (
+        <Hyperlink href={item.source} className=" inline-block mt-2 text-sm ">
+          Recipe Source
+        </Hyperlink>
+      )}
+    </div>
+  );
+};
+
 const ImageCard: FunctionComponent<Props> = ({ item }: Props) => {
   const { url, width, height } = item.image;
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div className="mb-6 sm:mb-8">
+    <div className={isOpen ? "mb-6 sm:mb-6" : "mb-4"}>
       <Image src={url} width={width} height={height} layout="responsive" objectFit="cover" alt={item.title} />
-      <div className="mt-3">
-        <p className="text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
-          {item.dateCreated.toLocaleDateString("en-US")}
-        </p>
-        <p className="mt-2 text-lg leading-6 font-semibold">{item.title}</p>
-        {item.description != null && (
-          <p className="mt-2 text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
-            {item.description}
-          </p>
-        )}
-        {item.source != null && (
-          <Hyperlink href={item.source} className=" inline-block mt-2 text-sm ">
-            Recipe Source
-          </Hyperlink>
-        )}
-      </div>
+      {isOpen && <Details item={item} />}
     </div>
   );
 };
