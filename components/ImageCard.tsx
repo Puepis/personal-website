6f6b0de1852061520b24e96d9feb15571b4f5065
Philips Xu <philips.ilips@gmail.com>
2022-01-08 15:25:25 -0500

    implement card animation

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { FunctionComponent, useState } from "react";
 import { FoodEntry } from "../src/types/FoodEntry";
 import FadeInImage from "./FadeInImage";
@@ -8,20 +9,24 @@ type Props = {
 };
 
 const Details: FunctionComponent<Props> = ({ item }: Props) => {
+  const separator = " • ";
+  const ingredients = item?.description?.split(",").map((x) => x.trim());
+  console.log(ingredients);
+
   return (
     <div className="mt-3">
-      <p className="text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
+      {/* <p className="text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
         {item.dateCreated.toLocaleDateString("en-US")}
-      </p>
-      <p className="mt-2 text-lg leading-6 font-semibold">{item.title}</p>
-      {item.description != null && (
+      </p> */}
+      <p className="mt-2 text-lg leading-6 font-semibold">{item.title.toLowerCase()}</p>
+      {ingredients && (
         <p className="mt-2 text-sm text-light-secondary-text dark:text-white dark:text-opacity-70">
-          {item.description}
+          {ingredients.join(separator)}
         </p>
       )}
       {item.source != null && (
         <Hyperlink href={item.source} className=" inline-block mt-2 text-sm ">
-          Recipe Source
+          source
         </Hyperlink>
       )}
     </div>
@@ -31,11 +36,27 @@ const Details: FunctionComponent<Props> = ({ item }: Props) => {
 const ImageCard: FunctionComponent<Props> = ({ item }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleOpen = () => setIsOpen(!isOpen);
+
   return (
-    <div className={isOpen ? "mb-6 sm:mb-6" : "mb-4 rounded-lg overflow-hidden"}>
-      <FadeInImage item={item} />
-      {isOpen && <Details item={item} />}
-    </div>
+    <motion.li layout className={isOpen ? "mb-6 sm:mb-6" : "mb-4"}>
+      <motion.div layout className="rounded-lg overflow-hidden" onClick={toggleOpen}>
+        <FadeInImage item={item} />
+      </motion.div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.1 }}
+          >
+            <Details item={item} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.li>
   );
 };
 
--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -1,3 +1,4 @@
+import { AnimateSharedLayout, motion } from "framer-motion";
 import type { NextPage } from "next";
 import ImageCard from "../components/ImageCard";
 import { FoodEntry } from "../src/types/FoodEntry";
@@ -12,17 +13,19 @@ const ImageGallery: NextPage<Props> = ({ items }: Props) => {
   const numColumns = width < 640 ? 2 : 3;
   const arr = Array.from({ length: numColumns }, (v, i) => i);
   return (
-    <footer className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4">
+    <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4">
       {arr.map((_, i) => (
-        <footer key={i}>
-          {items
-            .filter((_, index) => index % numColumns === i)
-            .map((item) => (
-              <ImageCard key={item.image.id} item={item} />
-            ))}
-        </footer>
+        <AnimateSharedLayout key={i}>
+          <motion.ul layout>
+            {items
+              .filter((_, index) => index % numColumns === i)
+              .map((item) => (
+                <ImageCard key={item.image.id} item={item} />
+              ))}
+          </motion.ul>
+        </AnimateSharedLayout>
       ))}
-    </footer>
+    </div>
   );
 };
 
