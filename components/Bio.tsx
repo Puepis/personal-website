aa03bd5f27764ac5b1f70a42a6413e80a9b4f08c
Philips Xu <philips.ilips@gmail.com>
2022-08-13 13:39:58 -0400

    update bio

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -4,16 +4,17 @@ import Hyperlink from "./Hyperlink";
 const Bio: FunctionComponent = () => {
   return (
     <p className="text-sm sm:text-base text-light-secondary-text dark:text-white dark:text-opacity-70 text-left">
-      Hey there! I’m a third-year Computer Science student at the University of Waterloo who loves to read about tech
-      and discover cool software!
+      Hey there! I’m a 3rd year Computer Science student at the University of Waterloo, passionate about building
+      reliable and user-friendly software. I love reading about new technologies and seeing how things are done at
+      scale!
       <br />
       <br />
-      Outside of school, I’ve had the opportunity to learn and grow at Wish &{" "}
+      Outside of school, I’ve had the opportunity to learn and grow at BitGo, Wish &{" "}
       <Hyperlink className="" href="https://uwblueprint.org/">
         UW Blueprint
       </Hyperlink>
-      . When I’m not trying to wrap my head around software, I’m either reading manga, watching food videos on YouTube,
-      or cooking something new!
+      . When I’m not trying to wrap my head around software, I’m probably cooking, researching recipes, or relaxing to
+      music!
     </p>
   );
 };
